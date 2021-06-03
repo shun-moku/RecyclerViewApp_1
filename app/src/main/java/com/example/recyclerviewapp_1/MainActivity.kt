f7b522f5d134393bc68b2b6358d646d6454d340d
shun-moku <prmnmbr13.blue828@gmail.com>
2021-06-03 18:47:14 +0900

    recyclerViewにadapterとlayoutManagerをセット

--- app/src/main/java/com/example/recyclerviewapp_1/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewapp_1/MainActivity.kt
@@ -1,11 +1,51 @@
 package com.example.recyclerviewapp_1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val numberList = listOf(
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9"
+        )
+        recyclerView = findViewById(R.id.recycler_view)
+
+        recyclerView.adapter = RecyclerViewAdapter(numberList)
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
